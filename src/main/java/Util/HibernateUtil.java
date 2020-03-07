b63fad29240eb866c7774a5c38d1f9a1581f2679
Pop1993 <cristian.pop1993@gmail.com>
2020-03-07 13:23:49 +0200

    Added git ignore file and set up for hibernate.

---
+++ src/main/java/Util/HibernateUtil.java
@@ -0,0 +1,39 @@
+package Util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory = null;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/class_project_group_1?useSSL=false");
+            settings.put(Environment.USER, "root2");
+            settings.put(Environment.PASS, "Root1234");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "update");
+            configuration.setProperties(settings);
+
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+
+    public static Session getSession() {
+        return getSessionFactory().openSession();
+    }
+}
