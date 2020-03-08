1603909c25519ad9448bdf491b6b6f8dc1a715c3
Pop1993 <cristian.pop1993@gmail.com>
2020-03-08 11:09:46 +0200

    Added Hibernate dependency

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,14 +1,22 @@
 package sample;
 
+import Util.UserService;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class Controller {
-
     @FXML
     private TextField username;
     @FXML
     private PasswordField password;
+
+    public void loginButtonIsClicked () {
+        if(UserService.doesUserExists(username.getText(), password.getText())){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setContentText("You have logged in succesfully");
+            alert.show();
+    } }
 
 }
