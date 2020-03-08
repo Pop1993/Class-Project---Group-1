5674fcbfbbed230a47147a4f2dbdb33f3f7f9999
Daniela Tuia <daniela.pintilie@yahoo.com>
2020-03-08 15:11:01 +0200

    Create fxml Shop for customer. Add sqlProduct Queries.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/LogInPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Buna ziua");
         primaryStage.setScene(new Scene(root, 500, 150));
         primaryStage.show();
