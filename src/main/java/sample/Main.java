c495748d191f45fc682e7c6bd07e9a34bc9a4310
Pop1993 <cristian.pop1993@gmail.com>
2020-03-13 20:47:46 +0200

    Added Hibernate dependency

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/LogInPage2.fxml"));
         primaryStage.setTitle("D.O.C. Online Shop");
-        primaryStage.setScene(new Scene(root, 500, 150));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
